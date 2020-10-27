939a792e6dbf5d57101b2165e442c3130e4d3df1
王安文 <8033695+wang-anwen@user.noreply.gitee.com>
2020-10-27 15:24:41 +0800

    解决冲突app

--- src/App.vue
+++ src/App.vue
@@ -13,25 +13,6 @@ export default {
 }
 </script>
 
-<<<<<<< HEAD
-<style scoped>
-=======
 <style>
->>>>>>> 8a2ec9e3a846a56b48420cda7751d47fe3da3a96
-*{
-  margin: 0;
-  padding: 0;
-}
-<<<<<<< HEAD
-=======
-html,body{
-  height: 100%;
-}
-body{
-  font-size: 16px;
-}
-#app{
-  margin-bottom: 0.5rem;
-}
->>>>>>> 8a2ec9e3a846a56b48420cda7751d47fe3da3a96
+
 </style>
