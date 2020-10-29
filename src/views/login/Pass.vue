aefbccd34b5c2ee5b2726f7818e67bb6f17e8e01
张鑫伟 <7956923+zxwqn@user.noreply.gitee.com>
2020-10-29 15:06:31 +0800

    手机号正则验证

--- src/views/login/Pass.vue
+++ src/views/login/Pass.vue
@@ -1,55 +1,57 @@
 <template>
   <div>
-      <div class="passWord_nav">
-<van-nav-bar
-  title="找回密码"
->
- <template #left>
-
-  <van-icon name="arrow-left"  size="22" color="black"  @click="toLogins" />
-  </template>
-</van-nav-bar>
-</div>
-<div class="passWord_from">
+    <div class="passWord_nav">
+      <van-nav-bar title="找回密码">
+        <template #left>
+          <van-icon
+            name="arrow-left"
+            size="22"
+            color="black"
+            @click="toLogins"
+          />
+        </template>
+      </van-nav-bar>
+    </div>
+    <div class="passWord_from">
       <van-form validate-first @failed="onFailed">
-  <!-- 通过 pattern 进行正则校验 -->
-  <van-field
-    v-model="value1"
-    name="pattern"
-    placeholder="请输入手机号"
-    :rules="[{ pattern, message: '请输入正确内容' }]"
-  />
-  <!-- 通过 validator 进行函数校验 -->
-  <van-field
-    v-model="value2"
-    name="validator"
-    placeholder="请输入验证码"
-    :rules="[{ validator, message: '请输入正确内容' }]"
-  />
-  <!-- 通过 validator 进行异步函数校验 -->
-  <van-field
-    v-model="value3"
-    name="asyncValidator"
-    placeholder="请输入密码"
-    :rules="[{ validator: asyncValidator, message: '请输入正确内容' }]"
-  />
-  <div style="margin: 0.8rem;margin-top:1rem;">
-    <van-button round block type="warning" native-type="submit">
-     确定
-    </van-button>
-  </div>
-</van-form>
-</div>
+        <!-- 通过 pattern 进行正则校验 -->
+        <van-field
+          v-model="value1"
+          name="pattern"
+          placeholder="请输入手机号"
+          :rules="[{ pattern, message: '请输入正确内容' }]"
+        />
+        <!-- 通过 validator 进行函数校验 -->
+        <van-field
+          v-model="value2"
+          name="validator"
+          placeholder="请输入验证码"
+          :rules="[{ validator, message: '请输入正确内容' }]"
+        />
+        <!-- 通过 validator 进行异步函数校验 -->
+        <van-field
+          v-model="value3"
+          name="asyncValidator"
+          placeholder="请输入密码"
+          :rules="[{ validator: asyncValidator, message: '请输入正确内容' }]"
+        />
+        <div style="margin: 0.8rem; margin-top: 1rem">
+          <van-button round block type="warning" native-type="submit">
+            确定
+          </van-button>
+        </div>
+      </van-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-data() {
+  data() {
     return {
-      value1: '',
-      value2: '',
-      value3: '',
+      value1: "",
+      value2: "",
+      value3: "",
       pattern: /\d{6}/,
     };
   },
@@ -61,7 +63,7 @@ data() {
     // 异步校验函数返回 Promise
     asyncValidator(val) {
       return new Promise((resolve) => {
-        Toast.loading('验证中...');
+        Toast.loading("验证中...");
 
         setTimeout(() => {
           Toast.clear();
@@ -70,18 +72,17 @@ data() {
       });
     },
     onFailed(errorInfo) {
-      console.log('failed', errorInfo);
+      console.log("failed", errorInfo);
     },
     // 跳转至登录页
-    toLogins(){
-this.$router.push({
-    path:"/login"
-})
-    }
+    toLogins() {
+      this.$router.push({
+        path: "/login",
+      });
+    },
   },
-}
+};
 </script>
 
 <style>
-
 </style>
