8aa520b133884754293c3442983d94d745257143
张祎祎 <>
2020-10-29 14:11:28 +0800

    我的页面调整

---
+++ src/views/mine/Ddxq.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+<div class="tobu">
+    
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/mine/Message/message.vue
+++ src/views/mine/Message/message.vue
@@ -9,11 +9,58 @@
 
 
         <!-- 空状态 主要内容 -->
-    <van-empty
+    <!-- <van-empty
       class="custom-image"
       image="https://wap.365msmk.com/img/empty.0d284c2e.png"
       description="暂无消息"
-    />
+    /> -->
+     <div>
+          <ul class="uuui">
+              <li class="uu-li">
+                  <img src="../../../assets/xiaoxi/youjian.png" alt="">
+              </li>
+              <li class="uu-li">
+                  <p>课程通知</p>
+                  <p class="uu-p">你报名的课程《李老师18号20号地理大课堂开课<br>啦》已经有3天没有学习了，快去学习吧</p>
+              </li>
+          </ul>
+           <ul class="uuui">
+              <li class="uu-li">
+                  <img src="../../../assets/xiaoxi/youjian.png" alt="">
+              </li>
+              <li class="uu-li">
+                  <p>系统通知</p>
+                  <p class="uu-p">暂无消息</p>
+              </li>
+          </ul>
+           <ul class="uuui">
+              <li class="uu-li">
+                  <img src="../../../assets/xiaoxi/shezi.png" alt="">
+              </li>
+              <li class="uu-li">
+                  <p>订单通知</p>
+                  <p class="uu-p">你的课程订单《》取消成功</p>
+              </li>
+          </ul>
+           <ul class="uuui">
+              <li class="uu-li">
+                  <img src="../../../assets/xiaoxi/che.png" alt="">
+              </li>
+              <li class="uu-li">
+                  <p>约课通知</p>
+                  <p class="uu-p">暂无消息</p>
+              </li>
+          </ul>
+           <ul class="uuui">
+              <li class="uu-li">
+                  <img src="../../../assets/xiaoxi/youjian.png" alt="">
+              </li>
+              <li class="uu-li">
+                  <p>考试通知</p>
+                  <p class="uu-p">暂无消息</p>
+              </li>
+          </ul>
+      </div>
   </div>
 </template>
 
@@ -28,4 +75,25 @@ export default {
 </script>
 
 <style>
+.uuui{
+    width: 100%;
+    display: inline-flex;
+    color: grey;
+}
+.uuui img{
+    width: 3rem;
+    height: 3rem;
+    border-radius:50% ;
+    position: relative;
+    top:0.3rem
+}
+.uu-li{
+    margin-left: 1rem;
+    margin-top: 0.5rem;
+}
+.uu-p{
+    font-size:0.5rem ;
+}
+
 </style>
+
