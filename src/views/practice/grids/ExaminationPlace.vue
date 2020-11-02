ecd92e414fe705a108f2f14f7996cfbffdfa37ae
张鑫伟 <7956923+zxwqn@user.noreply.gitee.com>
2020-11-02 20:52:34 +0800

    no message

--- src/views/practice/grids/ExaminationPlace.vue
+++ src/views/practice/grids/ExaminationPlace.vue
@@ -13,16 +13,26 @@
     </div> -->
 
     <!-- 分类 二级导航 -->
-    <van-sidebar v-model="activeKey">
-      <van-sidebar-item title="求导题库" />
+    <!-- <van-sidebar v-model="activeKey">
+      <van-sidebar-item title="求导题库" >
+        <template>
+          <div>21323323ew</div>
+        </template>
+      </van-sidebar-item>
       <van-sidebar-item title="极限题库" />
       <van-sidebar-item title="生物" />
       <van-sidebar-item title="化学" />
       <van-sidebar-item title="数学" />
       <van-sidebar-item title="物理" />
       <van-sidebar-item title="英语" />
-    </van-sidebar>
-    
+    </van-sidebar> -->
+
+    <van-tabs v-model="active" >
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
   </div>
 </template>
 <script>
@@ -34,7 +44,8 @@ export default {
   data() {
     return {
       value: "", //搜索关键字
-      activeKey: 0, //索引值
+      // activeKey: 0, //索引值
+      active: 2,
     };
   },
 
